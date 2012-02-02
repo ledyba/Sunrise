a8113f6c0c8662514199e009d3d4cf25850017b8
psi <psi@7io.org>
2012-02-02 21:13:16 +0900

    rubyっていうコマンドにした

--- rakefile.rb
+++ rakefile.rb
@@ -2,14 +2,13 @@
 require 'rake/clean'
 
 RACC="racc"
-RUBY="ruby"
 
 SRCS = FileList["**/*.racc"]
 GENS = SRCS.ext("rb");
 task :default => "test"
 
 task "test" => GENS do
-	sh "#{RUBY} main.rb test.asm"
+	ruby "main.rb", "test.asm"
 end
 
 task "reload" => ["clean", "test"]
