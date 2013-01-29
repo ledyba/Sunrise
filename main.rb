348a2e9a45ca5ac9b7df25e41edf9b18c9ab9985
psi <psi@7io.org>
2013-01-29 10:47:03 +0900

    add comment

--- main.rb
+++ main.rb
@@ -5,7 +5,20 @@ require File.dirname(__FILE__)+"/parser/parser.rb"
 require File.dirname(__FILE__)+"/asm/compiler.rb"
 require File.dirname(__FILE__)+"/asm/nes.rb"
 
+def usage()
+	puts "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
+	puts "Sunrise - a 6502 Assembler and linker for NES."
+	puts "                                 2011-2013 PSI"
+	puts "http://ledyba.org/famicom"
+	puts "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
+	puts "usage: ruby main.rb [*.asm].... [*.chr] [*.nes]"
+end
+
 def main(argv)
+	if argv.size < 3
+		usage()
+		return 1
+	end
 	parser = Parser.new();
 	compiler = ::Asm::Compiler.new(::Asm::Nes);
 	ARGV[0..-3].each(){|src|
@@ -14,6 +27,7 @@ def main(argv)
 		}
 	};
 	compiler.compile(ARGV[-2]).save(ARGV[-1]);
+	return 0
 end
 
-main(ARGV);
+exit(main(ARGV));
